fbefdea5df41a478c1852ed26afd3125eddcb90c
Maksym Kozlovskyi <zxcshaft@gmail.com>
2025-01-12 23:57:42 +0200

    Website2 v1

---
+++ rezyx/main/models.py
@@ -0,0 +1,36 @@
+from email.policy import default
+
+from django.db import models
+from django.urls import reverse
+
+class Category:
+    name = models.CharField(max_length=20,
+                            unique=True)
+    slug = models.SlugField(max_lenght=20,
+                            unique=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delate=models.CASCADE)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    prise = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.DecimalField(default=0.00, max_digits=4,
+                                   decimal_places=2)
