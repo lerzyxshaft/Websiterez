4fb1472e05f6d2c5f69aeb99baf44c546aff2144
Maksym Kozlovskyi <zxcshaft@gmail.com>
2025-01-17 17:44:46 +0200

    Website2 v1

--- rezyx/main/models.py
+++ rezyx/main/models.py
@@ -1,9 +1,10 @@
+from dataclasses import fields
 from email.policy import default
 
 from django.db import models
 from django.urls import reverse
 
-class Category:
+class Category(models.Model):
     name = models.CharField(max_length=20,
                             unique=True)
     slug = models.SlugField(max_lenght=20,
@@ -34,3 +35,23 @@ class Product(models.Model):
     updated = models.DateTimeField(auto_now=True)
     discount = models.DecimalField(default=0.00, max_digits=4,
                                    decimal_places=2)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created'])
+        ]
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('main:product_detail',
+                       args=[self.slug])
+
+    def sell_price(self):
+        if self.discount:
+            return round(self.price - self.price * self.discount / 100, 2)
+        return self.price
