4e1ae0b5d18d15e377b693b7c27cd1275df4f370
Maksym Kozlovskyi <zxcshaft@gmail.com>
2025-02-17 23:50:34 +0100

    Website2 v1 17feb

--- rezyx/main/views.py
+++ rezyx/main/views.py
@@ -2,7 +2,7 @@ from zoneinfo import available_timezones
 
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
-
+from django.core.paginator import Paginator
 
 def popular_list(request):
     products = Product.objects.filter(available = True)[:3]
@@ -21,15 +21,20 @@ def product_detail(request, slug):
                   {'product': product})
 
 def product_list(request, category_slug=None):
+    page = request.Get('page', 1)
     category = None
     categories = Category.objects.all
+    paginator = Paginator(products, 1)
+    current_page = paginator.page(int(page))
     products = Product.objects.filter(available = True)
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
-        products = products.filter(category=category)
+        paginator = Paginator(products.filter(category=category), 1)
+        current_page = paginator.page(int(page))
     return render(request,
                   'main/product/list.html',
                     {"category": category,
                     'categories': categories,
-                    'products': products})
+                    'products': current_page,
+                     'slug_url': category_slug})
